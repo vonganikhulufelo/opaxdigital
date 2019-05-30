55d5abc5fcacfd8c3b3ab15de48d7a53a258aa75
vongani <vonganikhulufelo@gmail.com>
2019-05-30 10:53:10 +0200

    select2

--- app/models/purchase_order.rb
+++ app/models/purchase_order.rb
@@ -1,2 +1,5 @@
 class PurchaseOrder < ApplicationRecord
+	  belongs_to :user
+  has_many :purchase_order_products, dependent: :destroy
+  accepts_nested_attributes_for :purchase_order_products,  reject_if: :all_blank, allow_destroy: true
 end
--- app/models/purchase_order_product.rb
+++ app/models/purchase_order_product.rb
@@ -1,2 +1,4 @@
 class PurchaseOrderProduct < ApplicationRecord
+	belongs_to :purchase_order, optional: true
+  validates :zone_price, presence: true
 end
