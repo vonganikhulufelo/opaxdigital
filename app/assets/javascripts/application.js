2f4c6e43f7368f3796725aab8f65fc27daeb2235
vongani <vonganikhulufelo@gmail.com>
2019-05-30 10:57:46 +0200

    select2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery3
+//= require jquery
 //= require shieldui.min
 //= require tree.min
 //= require rails-ujs
