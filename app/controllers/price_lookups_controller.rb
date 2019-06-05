473e4dccab14b9fc4d92157e9a975861045d9c72
vongani <vonganikhulufelo@gmail.com>
2019-06-05 15:33:56 +0200

    price look up date

--- app/controllers/price_lookups_controller.rb
+++ app/controllers/price_lookups_controller.rb
@@ -1,6 +1,6 @@
 class PriceLookupsController < ApplicationController
     def index
-        @price_lookups = current_user.zones.joins(:magisterial_districts, :product_prices).select('magisterial_districts.zonedistrict as zone_district, magisterial_districts.magisterialdistrict_code as magisterial_code, product_prices.productprice_price as product_price, product_prices.productdescription_product as product_description')
-        .group('magisterial_districts.zonedistrict, magisterial_districts.magisterialdistrict_code, magisterial_districts.magisterialdistrict_province, product_prices.productprice_price, product_prices.productdescription_product')
+        @price_lookups = current_user.zones.joins(:magisterial_districts, :product_prices).select('magisterial_districts.zonedistrict as zone_district, magisterial_districts.magisterialdistrict_code as magisterial_code, product_prices.productprice_price as product_price, product_prices.productdescription_product as product_description, product_prices.uid as uid,product_prices.updated_at  as updated_at')
+        .group('magisterial_districts.zonedistrict, magisterial_districts.magisterialdistrict_code, magisterial_districts.magisterialdistrict_province, product_prices.productprice_price, product_prices.productdescription_product,product_prices.uid,product_prices.updated_at')
     end
   end
