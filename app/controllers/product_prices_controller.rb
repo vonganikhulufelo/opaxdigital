d2b578722cd32b9aa54f8fb8f0d9278be3e9a614
vongani <vonganikhulufelo@gmail.com>
2019-06-05 13:11:40 +0200

    df

--- app/controllers/product_prices_controller.rb
+++ app/controllers/product_prices_controller.rb
@@ -45,6 +45,8 @@ def import
           
           Log.create!(description: @dis, username: current_user.name, uid: @ppp.uid.to_s,user_id: current_user.user_id)
         else
+          @dis12 = row[0].to_s + '$' + row[1].to_s + '$' + row[2].to_s + '$' + row[3].to_s
+          @log = Log.find_by(description: @dis12)
 
           if !@log.present?
          @product_price.update(productprice_price: row[3])
@@ -59,7 +61,7 @@ def import
             @supplier_price.each do | supplier |
               supplier.update(net_price: @product_price1.productprice_price + supplier.product_rebate,product_price_price: @product_price1.productprice_price)
               @net = @product_price1.productprice_price + supplier.product_rebate
-              @description = supplier.created_at.strftime("%Y-%m-%d %H:%M:%S").to_s + "$" + supplier.productprice_price.to_s + "$" + supplier.product_rebate.to_s+ "$" + @net.to_s
+              @description = supplier.created_at.strftime("%Y-%m-%d %H:%M:%S").to_s + "$" + @product_price1.productprice_price.to_s + "$" + supplier.product_rebate.to_s+ "$" + @net.to_s
               Log.create!(user_id: current_user.user_id,description: @description, username: current_user.name, uid: supplier.uid.to_s)
             end
           end
@@ -86,8 +88,8 @@ end
   # POST $product_prices
   # POST $product_prices.json
   def create
-   @check_product = ProductPrice.find_by(zone_id: params[:zone_id],product_description_id: params[:product_discription_id])
-    if !@check_product.present?
+   @check_product = ProductPrice.find_by(product_description_id: params[:product_description_id], zone_id: params[:zone_id])
+    if !@check_product
       @product_description = ProductDescription.find(params[:product_discription_id])
       @zone = Zone.find(params[:zone_id])
       @product_price = ProductPrice.new(user_id: current_user.user_id,product_description_id: params[:product_discription_id], productprice_month_id: params[:month_id], zone_id: params[:zone_id], productprice_price: params[:productprice_price],productdescription_product: @product_description.productdescription_product,productprice_zone_class: @zone.zone_description)
@@ -110,7 +112,7 @@ end
         @zone = Zone.find(params[:zone_id])
   
         if @check_product.update(product_description_id: params[:product_discription_id], productprice_month_id: params[:month_id], zone_id: params[:zone_id], productprice_price: params[:productprice_price],productdescription_product: @product_description.productdescription_product,productprice_zone_class: @zone.zone_description)
-          @description = params[:month_id].strftime("%Y/%m/%d").to_s + '$' + @zone.zone_description.to_s + '$' + @product_description.productdescription_product.to_s + '$' + params[:productprice_price].to_s
+          @description = params[:month_id] + '$' + @zone.zone_description + '$' + @product_description.productdescription_product.to_s + '$' + params[:productprice_price].to_s
           Log.create!(description:@description, username: current_user.name, uid: @check_product.uid,user_id: current_user.user_id)
           @product_price1 = ProductPrice.find(@check_product.id)
           @supplier_price = SPayMProductPrice.where(product_price_id: @check_product.id)
@@ -130,8 +132,7 @@ end
               @supp_price = SPayMProductPrice.find(customer.s_pay_m_product_price_id)
               @gross = (@product_price1.productprice_price + customer.product_rebate) - @supp_price.net_price
               customer.update(s_net_price: @supp_price.net_price,gross_price: @gross,net_price: @product_price1.productprice_price + customer.product_rebate,product_price_price: @product_price1.productprice_price)
-              @customer_rebate = CPayMProductPrice.find(customer.id)
-              @description1 = @customer_rebate.updated_at.strftime("%Y-%m-%d %H:%M:%S").to_s + "$" + @customer_rebate.product_pescription_product.to_s + "$" + @customer_rebate.product_price_price.to_s+ "$" + @customer_rebate.product_rebate.to_s + "$" + @customer_rebate.claw_margin.to_s + "$" + @customer_rebate.net_price.to_s + "$" + @customer_rebate.s_name.to_s + "$" + @customer_rebate.s_net_price.to_s + "$" + @customer_rebate.gross_price.to_s
+              @description1 = customer.created_at.strftime("%Y-%m-%d %H:%M:%S").to_s + "$" +  @product_price1.productprice_price.to_s + "$" + customer.product_rebate.to_s+ "$" +  @supp_price.net_price.to_s
               Log.create!(user_id: current_user.user_id,description: @description1, username: current_user.name, uid: customer.uid.to_s)
             end
           end
@@ -142,7 +143,7 @@ end
           format.json { render json: @product_price.errors, status: :unprocessable_entity }
         end
       end
-      
+      redirect_to product_prices_url
     end
   end
 
@@ -174,8 +175,7 @@ end
               @supp_price = SPayMProductPrice.find(customer.s_pay_m_product_price_id)
               @gross = (@product_price1.productprice_price + customer.product_rebate) - @supp_price.net_price
               customer.update(s_net_price: @supp_price.net_price,gross_price: @gross,net_price: @product_price1.productprice_price + customer.product_rebate,product_price_price: @product_price1.productprice_price)
-              @customer_rebate = CPayMProductPrice.find(customer.id)
-              @description1 = @customer_rebate.updated_at.strftime("%Y-%m-%d %H:%M:%S").to_s + "$" + @customer_rebate.product_pescription_product.to_s + "$" + @customer_rebate.product_price_price.to_s+ "$" + @customer_rebate.product_rebate.to_s + "$" + @customer_rebate.claw_margin.to_s + "$" + @customer_rebate.net_price.to_s + "$" + @customer_rebate.s_name.to_s + "$" + @customer_rebate.s_net_price.to_s + "$" + @customer_rebate.gross_price.to_s
+              @description1 = customer.created_at.strftime("%Y-%m-%d %H:%M:%S").to_s + "$" +  @product_price1.productprice_price.to_s + "$" + customer.product_rebate.to_s+ "$" +  @supp_price.net_price.to_s
               Log.create!(user_id: current_user.user_id,description: @description1, username: current_user.name, uid: customer.uid.to_s)
             end
           end
