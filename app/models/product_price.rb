03714be67bcac204cc3b96ef94dcfc7700fe53ea
vongani <vonganikhulufelo@gmail.com>
2019-06-14 15:22:33 +0200

    It adds toastr on product price

--- app/models/product_price.rb
+++ app/models/product_price.rb
@@ -4,7 +4,7 @@ class ProductPrice < ApplicationRecord
   belongs_to :zone
   has_many :s_pay_m_product_prices, dependent: :destroy
   has_many :c_pay_m_product_prices, dependent: :destroy
-  after_destroy :create_logs
+  before_destroy :create_logs
 
   def create_logs
   	@log = Log.where(uid: self.uid).last
