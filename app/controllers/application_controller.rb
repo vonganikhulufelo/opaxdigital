0703ed1036882a30332f8e34377a1c7816bdb412
vongani <vonganikhulufelo@gmail.com>
2019-07-09 08:55:24 +0200

    steps order

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
 	
 	private
 	def not_authenticated
-	  redirect_to login_path, alert: "Please login first"
+	  redirect_to login_path, flash: { danger: "Please login first" }
 	end
 end
