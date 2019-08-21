d6125a102e4b74552b64938b2830fd3c4f83206c
vongani <vonganikhulufelo@gmail.com>
2019-08-21 16:10:34 +0200

    sales order

--- app/controllers/sales_orders_controller.rb
+++ app/controllers/sales_orders_controller.rb
@@ -1,7 +1,8 @@
 class SalesOrdersController < ApplicationController
   def index
-    @sales = SalesOrder.where(user_id: current_user.user_id).search(params[:search]).paginate(page: params[:page], per_page: 50).order('customer_name ASC')
-    @sales1 = sales_sql
+    @sales1 = SalesOrder.where(user_id: current_user.user_id).search(params[:search]).paginate(page: params[:page], per_page: 50).order('customer_name ASC')
+    @sales = sales_sql
+    @count = sales_sql.count
   end
 
   def new
@@ -28,6 +29,9 @@ class SalesOrdersController < ApplicationController
   def edit
     @sales_order = SalesOrder.find_by(id: params[:id], user_id: current_user.user_id)
     @customers = Customer.where(user_id: current_user.user_id)
+    @payemnts = CustomerPaymentTerm.where(customer_id: @sales_order.customer_id)
+    @zones = CPayMDistrict.where(customer_payment_term_id: @sales_order.payment_id)
+    @product = CPayMProductPrice.where(c_pay_m_district_id: @sales_order.zone_id)
   end
 
   def step2
@@ -61,10 +65,10 @@ class SalesOrdersController < ApplicationController
   private
 
   def sales_sql
-    ::User.find(current_user.user_id).sales_orders.joins(:sales_order_products).select('sales_orders.customer_name as customer_name, sales_orders.status as status, sales_orders.zone as zone, sales_orders.order_date as order_date, sales_orders.payment as payment, sales_orders.zone as zone, sales_orders.invoice as invoice, sales_orders.recon as recon, sales_orders.delivery_date_on_delivery as estimated_delivery_date, sales_orders.delivery_note_reference as delivery_note,
+    ::User.find(current_user.user_id).sales_orders.joins(:sales_order_products).select('COUNT(*) as count,sales_orders.updated_at,sales_orders.customer_name as customer_name, sales_orders.status as status, sales_orders.zone as zone, sales_orders.order_date as order_date, sales_orders.payment as payment, sales_orders.zone as zone, sales_orders.invoice as invoice, sales_orders.recon as recon, sales_orders.delivery_date_on_delivery as estimated_delivery_date, sales_orders.delivery_note_reference as delivery_note,
       sales_order_products.product_name as product_name, sales_order_products.order_rate as order_rate, sales_order_products.order_quantity as order_quantity, sales_order_products.order_value as order_value, sales_order_products.delivered_date as product_delivered_date, sales_order_products.delivery_quantity as delivery_quantity, sales_order_products.delivery_rate as delivery_price, sales_order_products.delivery_value as delivery_value,sales_orders.id ')
-    .group('sales_orders.customer_name, sales_orders.status, sales_orders.zone, sales_orders.order_date, sales_orders.payment, sales_orders.zone, sales_orders.invoice, sales_orders.recon, sales_orders.delivery_date_on_delivery, sales_orders.delivery_date_on_delivery, sales_orders.delivery_note_reference, sales_order_products.product_name, sales_order_products.order_rate, sales_order_products.order_quantity, sales_order_products.order_value, sales_order_products.delivered_date,
-      sales_order_products.delivery_quantity, sales_order_products.delivery_rate, sales_order_products.delivery_value, sales_orders.id')
+    .group('sales_orders.updated_at,sales_orders.customer_name, sales_orders.status, sales_orders.zone, sales_orders.order_date, sales_orders.payment, sales_orders.zone, sales_orders.invoice, sales_orders.recon, sales_orders.delivery_date_on_delivery, sales_orders.delivery_date_on_delivery, sales_orders.delivery_note_reference, sales_order_products.product_name, sales_order_products.order_rate, sales_order_products.order_quantity, sales_order_products.order_value, sales_order_products.delivered_date,
+      sales_order_products.delivery_quantity, sales_order_products.delivery_rate, sales_order_products.delivery_value, sales_orders.id').search(params[:search]).paginate(page: params[:page], per_page: 50).order('sales_orders.updated_at ASC')
   end
 
   def customer_rebate_params
